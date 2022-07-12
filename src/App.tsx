06ec8585aec8e240380b505ff3aa42d493a77395
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-12 14:34:43 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 
 import { defaultTheme } from "./styles/themes/default";
 
@@ -12,6 +13,8 @@ function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
