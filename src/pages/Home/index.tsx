b4ab9ca038820459eaeb93718b9d1a87d5a82afb
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-18 12:54:43 -0300

    Funcionalidades da aplicação - Reduzindo countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,8 @@ import {
   StartCountdownButton,
   TaskInput
 } from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 function Home() {
@@ -43,13 +45,26 @@ function Home() {
     }
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
 
     const newCycle: Cycle = {
       id,
       task: data.task,
-      minutesAmount: data.minutesAmount
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
     };
 
     setCycles((state) => [...cycles, newCycle]);
@@ -57,8 +72,6 @@ function Home() {
 
     reset();
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
